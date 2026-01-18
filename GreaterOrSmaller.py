3cad2a7692fcdab01d5ca9c6bbed72781473a5bc
Hello78245 <shinimagesh10@gmail.com>
2026-01-17 19:27:50 -0800

    GreaterOrSmaller

---
+++ GreaterOrSmaller.py
@@ -0,0 +1,8 @@
+i=int(input("Enter a number"))
+if (i<50):
+    print("i is less than 50")
+    print("I'm in if Block")
+else:
+    print("i is greater than 50")
+    print("I'm in Else block")
+print("I'm not in if or else block")
